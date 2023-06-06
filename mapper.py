f0c7748825fc21ac581856260ae854a9af5c24f9
Eswar H4ck3r <76161608+eswarkumarofficial@users.noreply.github.com>
2023-06-06 18:23:27 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,32 +1,39 @@
 import requests
+from datetime import datetime
+import pyfiglet as pyfiglet
 from bs4 import BeautifulSoup
 
+ascii_banner = pyfiglet.figlet_format("H4ck3r - wavs")
+print(ascii_banner)
+print("                                                  "   + "by Eswar H4ck3r" + "\n")
+
+print("-" * 60)
+print(str(datetime.now()))
+print("-" * 60)
+
+print("\n<<<<<<<<<< Mapper >>>>>>>>>>\n")
 
 def map_web_application(url):
-    try:
-        # Send a GET request to the provided URL
-        response = requests.get(url)
+    # Send a GET request to the provided URL
+    response = requests.get(url)
 
-        response.raise_for_status()  # Raise an exception for non-200 status codes
+    # Extract links from the response
+    links = extract_links(response)
 
-        # Extract links from the response
-        links = extract_links(response)
-
-        # Write the discovered links to the report file
-        with open("Mapper.txt", "w") as report:
-            for link in links:
-                report.write(link + "\n")
-
-    except requests.exceptions.RequestException as e:
-        print("An error occurred while making the request:", e)
-    except Exception as e:
-        print("An error occurred:", e)
+    # Print the discovered links
+    for link in links:
+        print(link +"\n")
 
 
 def extract_links(response):
+    # Extract links from the response using a library like BeautifulSoup
+    # This example uses BeautifulSoup for HTML parsing
+    from bs4 import BeautifulSoup
+
     soup = BeautifulSoup(response.text, "html.parser")
     links = []
 
+    # Find all anchor tags in the HTML
     for anchor in soup.find_all("a"):
         href = anchor.get("href")
         if href:
@@ -36,5 +43,6 @@ def extract_links(response):
 
 
 # Example usage
-target_url = input("Enter the URL to Map: ")
+target_url = input("Enter the Website to Map : ")
 map_web_application(target_url)
+
