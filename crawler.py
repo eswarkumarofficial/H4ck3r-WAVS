aa9499b3191ec0b245cf1592c5096b4841ad01ef
Eswar H4ck3r <76161608+eswarkumarofficial@users.noreply.github.com>
2024-01-27 20:07:08 +0530

    crawler.py

--- crawler.py
+++ crawler.py
@@ -1,12 +1,13 @@
 import requests
-import pyfiglet as pyfiglet
+import pyfiglet
 from datetime import datetime
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from colorama import Fore, Style  # Import colorama for color support
 
 ascii_banner = pyfiglet.figlet_format("H4ck3r - wavs")
 print(ascii_banner)
-print("                                                  "   + "by Eswar H4ck3r" + "\n")
+print("                                                  " + "by Eswar H4ck3r" + "\n")
 
 print("-" * 60)
 print(str(datetime.now()))
@@ -28,16 +29,15 @@ def crawl(url):
             # Handle any request errors
             continue
 
+        print(f"Visited URL: {current_url}")
+        print(f"Status Code: {highlight_status_code(response.status_code)}\n")
+
         if response.status_code != 200:
             # Handle non-OK responses
             continue
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Process the current URL here...
-        print("Visited URL: " + current_url + "\n")
-
-        # Find and enqueue new URLs to visit
         links = soup.find_all('a')
         for link in links:
             href = link.get('href')
@@ -47,6 +47,18 @@ def crawl(url):
                     queue.append(absolute_url)
 
 
-# Example usage
+def highlight_status_code(status_code):
+    if 200 <= status_code < 300:
+        return f"{Fore.GREEN}{status_code}{Style.RESET_ALL}"  # Green for success
+    elif 300 <= status_code < 400:
+        return f"{Fore.BLUE}{status_code}{Style.RESET_ALL}"  # Blue for redirection
+    elif 400 <= status_code < 500:
+        return f"{Fore.YELLOW}{status_code}{Style.RESET_ALL}"  # Yellow for client errors
+    elif 500 <= status_code:
+        return f"{Fore.RED}{status_code}{Style.RESET_ALL}"  # Red for server errors
+    else:
+        return str(status_code)
+
+
 target_url = input("Enter the Website to Crawl: ")
 crawl(target_url)
