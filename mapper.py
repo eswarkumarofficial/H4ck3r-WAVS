c03ef5db1a21cfbfda1ce8a40742ed60d07a73e7
Eswar H4ck3r <76161608+eswarkumarofficial@users.noreply.github.com>
2024-03-14 08:45:37 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,11 +1,13 @@
 import requests
 from datetime import datetime
-import pyfiglet as pyfiglet
+import pyfiglet
 from bs4 import BeautifulSoup
+import os
+from urllib.parse import urljoin, urlparse
 
 ascii_banner = pyfiglet.figlet_format("H4ck3r - wavs")
 print(ascii_banner)
-print("                                                  "   + "by Eswar H4ck3r" + "\n")
+print("                                                  " + "by Eswar H4ck3r" + "\n")
 
 print("-" * 60)
 print(str(datetime.now()))
@@ -13,36 +15,59 @@ print("-" * 60)
 
 print("\n<<<<<<<<<< Mapper >>>>>>>>>>\n")
 
-def map_web_application(url):
+
+def map_web_application(url, save_to_file=False):
     # Send a GET request to the provided URL
     response = requests.get(url)
 
-    # Extract links from the response
-    links = extract_links(response)
+    # Extract file links from the response
+    file_links = extract_file_links(response, base_url=url)
 
-    # Print the discovered links
-    for link in links:
-        print(link +"\n")
+    # Print the discovered file links
+    for file_link in file_links:
+        print(file_link)
+
+    # Save file links to a text file if requested
+    if save_to_file:
+        save_links_to_file(file_links, url)
 
 
-def extract_links(response):
-    # Extract links from the response using a library like BeautifulSoup
-    # This example uses BeautifulSoup for HTML parsing
-    from bs4 import BeautifulSoup
-
+def extract_file_links(response, base_url):
+    # Extract file links from the response using BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
-    links = []
+    file_links = []
 
     # Find all anchor tags in the HTML
     for anchor in soup.find_all("a"):
         href = anchor.get("href")
         if href:
-            links.append(href)
+            full_url = urljoin(base_url, href)
+            if is_file_link(full_url):
+                file_links.append(full_url)
 
-    return links
+    return file_links
+
+
+def is_file_link(url):
+    # Check if the URL points to a file (you can customize this based on your file types)
+    file_extensions = ['.pdf', '.doc', '.txt', '.zip', '.jpg', '.png', '.exe', '.php']  # Add more if needed
+    _, extension = os.path.splitext(url)
+    return extension in file_extensions
+
+
+def save_links_to_file(links, url):
+    # Generate a filename based on the URL
+    filename = f"{urlparse(url).hostname}_mapper.txt"
+    
+    with open(filename, "w") as file:
+        file.write("\n".join(links))
+    print(f"\nFile links saved to '{filename}'")
 
 
 # Example usage
-target_url = input("Enter the Website to Map : ")
-map_web_application(target_url)
+target_url = input("Enter the Website to Map: ")
 
+# Set save_to_file to True
+save_to_file = True
+
+map_web_application(target_url, save_to_file)
